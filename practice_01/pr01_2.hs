043160323a927992558447ff0c0fd8da8e1460b1
Konstantin Gumnitskiy <gumnitskiyk@mail.ru>
2026-03-06 10:20:31 +0300

    Добавлены 3 функции второй части

--- practice_01/pr01_2.hs
+++ practice_01/pr01_2.hs
@@ -24,3 +24,33 @@ module Pr01_2 where
     -- Обработку недостатка или отсутствия ингредиентов
 
 -}
+
+myZipSave :: [a] -> [b] -> ([(a, b)], Either [a] [b])
+myZipSave [] [] = ([], Left [])
+myZipSave [] bs = ([], Right bs)
+myZipSave as [] = ([], Left as)
+myZipSave (a:as) (b:bs) = ((a,b):pairs, rest)
+    where (pairs, rest) = myZipSave as bs
+
+myUnzipSave :: ([(a, b)], Either [a] [b]) -> ([a], [b])
+myUnzipSave ([], Left restA) = (restA, [])
+myUnzipSave ([], Right restB) = ([], restB)
+myUnzipSave ((a,b):pairs, rest) = (a:as, b:bs)
+    where (as, bs) = myUnzipSave (pairs, rest)
+
+strictFoldl :: (b -> a -> b) -> b -> [a] -> b
+strictFoldl _ acc [] = acc
+strictFoldl f acc (x:xs) = let acc' = f acc x in acc' `seq` strictFoldl f acc' xs
+
+main :: IO()
+main = do
+    let list1 = [1, 2, 3, 4, 5]
+    let list2 = ["a", "b", "c"]
+    let list3 = [True, False]
+    print $ myZipSave list1 list2
+    print $ myZipSave list2 list3
+    
+    let zipped1 = myZipSave list1 list2
+    let zipped2 = myZipSave list2 list3
+    print $ myUnzipSave zipped1
+    print $ myUnzipSave zipped2
